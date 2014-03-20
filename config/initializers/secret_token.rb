25101630ed78b6fbb1b0edb195a40bb293c0dc9c
Yogibliz <peter.lunderbye@gmail.com>
2014-03-20 12:56:02 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MtFlow::Application.config.secret_key_base = 'ea25a2d32de3925e5e03416bcf295861c505fd35eff71721b66b03967d6edbc23c33c668eeedeba0fa24e2e79365356b572a8111ff38ba0c14407340da6085d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MtFlow::Application.config.session_store :cookie_store, key: '_mt_flow_session'
